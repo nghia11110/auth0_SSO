45e50d8ec0c8ab140ff2eaec7cae5b20baa78515
Nghia Nguyen <nghianguyen248@gmail.com>
2018-03-12 18:39:34 +0900

    add project

---
+++ SSO-App1/init.php
@@ -0,0 +1,17 @@
+<?php
+
+require "vendor/autoload.php";
+
+use Auth0\SDK\Auth0;
+
+$auth0 = new Auth0([
+  'domain' => 'eas-test.auth0.com',
+  'client_id' => 'MpCoImj9azpIKU2259fFXAXXcY1xJRYp',
+  'client_secret' => 'CtSyWxuT8P6KOj2RTQu1ZLNvN1HmiZKrDfE4bboLKBSfgZQbT6B41aEu3Qxt',
+  'redirect_uri' => 'http://localhost/sso-app1/callback',
+  'audience' => 'https://eas-test.auth0.com/userinfo',
+  'scope' => 'openid profile',
+  'persist_id_token' => true,
+  'persist_access_token' => true,
+  'persist_refresh_token' => true,
+]);
---
+++ SSO-App2/init.php
@@ -0,0 +1,17 @@
+<?php
+
+require "vendor/autoload.php";
+
+use Auth0\SDK\Auth0;
+
+$auth0 = new Auth0([
+  'domain' => 'eas-test.auth0.com',
+  'client_id' => 'xXdd77BzxKCzqQ84EfA1eg9vTs1KLMsX',
+  'client_secret' => '8RzlsNpaXmLG8qP8KMBUPqZh4EtzOHM2UeSXgQEfZjVvDkOVi4C618r6rGkMiG1o',
+  'redirect_uri' => 'http://localhost/sso-app2/callback',
+  'audience' => 'https://eas-test.auth0.com/userinfo',
+  'scope' => 'openid profile',
+  'persist_id_token' => true,
+  'persist_access_token' => true,
+  'persist_refresh_token' => true,
+]);
